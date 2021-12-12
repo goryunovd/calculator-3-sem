14c90bb448cd457c117835000f525d3408a6d38c
Горюнов Дмитрий <dsgoryunov@stud.etu.ru>
2021-12-12 20:24:10 +0300

    Add project files.

---
+++ calculator 3 sem/calculator 3 sem.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#define pi 3.14159265 
+#define exp 2.718281
+using namespace std;
+int check_for_correct_input(string str,bool check_for_bracekts)
+{
+	check_for_bracekts= 1;
+	int posL, posR;
+	string str_check_for_brackets=str;
+	string Lbracket = "(";
+	string Rbracket = ")";
+	//cout << "change sting:" << str_check_for_brackets << endl;
+	while (check_for_bracekts)
+	{
+		posL = str_check_for_brackets.find(Lbracket); 
+		posR = str_check_for_brackets.find(Rbracket);
+		//cout << "change sting:" << str_check_for_brackets << endl;
+		
+		if ((posL != -1) && (posR != -1) && (posL < posR))
+		{	
+			str_check_for_brackets.erase(posL, 1);
+			check_for_bracekts = 1;		
+
+		str_check_for_brackets.erase(posR-1, 1);
+		}
+		else
+			check_for_bracekts = 0;
+	}
+	
+	if (posL == posR) { check_for_bracekts = 1; }
+	return check_for_bracekts;
+};
+void polish_notation_isntmyorientation(string str) 
+{
+
+};
+/*
+int finding_corners(string str,char arithm,int leftcorner,int rightcorner)
+{
+		
+
+};*/
+void calculation(string str)
+{
+	string tmp_str;
+	int  left_point, right_point,i,j,pos_ar;//pos_ar- position of artihmetic left/right_point is for borders of double
+	double tmp_d1, tmp_d2;
+	cout << str << endl;
+	while (str.find("exp") != -1)
+	{
+		i = str.find("exp");//find index of 'e'
+		j = str.find("p", i);
+		tmp_str = to_string(exp);
+		str.replace(i, j - i + 1, tmp_str);
+		cout << str << endl;
+	}
+	while (str.find("pi") != -1)
+	{
+		i = str.find("pi");//find index of 'p'
+		j = str.find("i", i);
+		tmp_str = to_string(pi);
+		str.replace(i, j - i + 1, tmp_str);
+		cout << str << endl;
+	}
+	while (str.find("cos(")!=-1)
+	{
+		i = str.find("cos(");//find index of 'c'
+		j = str.find(")", i);
+		tmp_str.assign(str,i+4, j-(i+4));
+		tmp_d1 = std::stod(tmp_str);
+		//calculation cos of thi tmp_d1 and then add it 
+		tmp_d2 = cos(tmp_d1);
+		tmp_str = to_string(tmp_d2);
+		str.replace(i, j-i+1, tmp_str);
+		cout << str<<endl;
+	}
+	while (str.find("sin(") != -1)
+	{
+		i = str.find("sin(");//find index of 's'
+		j = str.find(")", i);
+		tmp_str.assign(str, i + 4, j - (i + 4));
+		tmp_d1 = std::stod(tmp_str);
+		//calculation sin of thi tmp_d1 and then add it 
+		tmp_d2 = sin(tmp_d1);
+		tmp_str = to_string(tmp_d2);
+		str.replace(i, j - i+1, tmp_str);
+		cout << str << endl;
+	}
+	while (str.find("sgn(") != -1)
+	{
+		i = str.find("sgn(");//find index of 's'
+		j = str.find(")", i);
+		tmp_str.assign(str, i + 4, j - (i + 4));
+		tmp_d1 = std::stod(tmp_str);
+		//calculation sin of thi tmp_d1 and then add it 
+		if (tmp_d1 > 0) tmp_d2 = 1;
+		if (tmp_d1 < 0) tmp_d2 = -1;
+		if (tmp_d1 == 0) tmp_d2 = 0;
+		tmp_str = to_string(tmp_d2);
+		str.replace(i, j - i+1,"("+ tmp_str+")");
+		cout << str << endl;
+	}
+	while (str.find("log(") != -1)
+	{
+		i = str.find("log(");//find index of 's'
+		j = str.find(")", i);
+		tmp_str.assign(str, i + 4, j - (i + 4));
+		tmp_d1 = std::stod(tmp_str);
+		//calculation sin of thi tmp_d1 and then add it 
+		tmp_d2 = log(tmp_d1);
+		tmp_str = to_string(tmp_d2);
+		str.replace(i, j - i + 1, tmp_str);
+		cout << str << endl;
+	}
+	//we make our calculation little easer lets find the nearest ) form the beginging bagining beginning 
+	//let it be that i is left corner index and j is right corner index
+
+	while (str.find("^")!=-1|| str.find("*") != -1|| str.find("+") != -1|| str.find("-") != -1|| str.find("/") != -1)
+	{
+		j = str.find(")");
+		i = str.rfind("(", j);
+		//if (str.find("*", j, i)!=-1&& str.find("/", j, i)==-1)
+		if (i != -1 && j != -1)
+		{
+			while (str.find("*", i, j) != -1 || str.find("+", i, j) != -1 || str.find("-", i, j) != -1 || str.find("/", i, j) != -1)
+				//in brackets 
+			{
+				pos_ar = str.find("*", j, i);
+				if (pos_ar != -1)
+				{//should find in brackets two doubles left and right to *
+					for (left_point = pos_ar; left_point > i; left_point--) {
+						if (str[left_point] == '+' || str[left_point] == '-' || str[left_point] == '/') { left_point++; break; }
+					}
+					tmp_str.assign(str, left_point, pos_ar);
+					tmp_d1 = std::stod(tmp_str);
+					for (right_point = pos_ar; right_point > i; right_point--) {
+						if (str[right_point] == '+' || str[right_point] == '-' || str[right_point] == '/') { right_point++; break; }
+					}
+
+					tmp_str.assign(str, pos_ar, right_point);
+					tmp_d2 = std::stod(tmp_str);
+					tmp_d2 = tmp_d2 * tmp_d1;
+					tmp_str = to_string(tmp_d2);
+					str.replace(left_point, right_point -left_point + 1, tmp_str);
+				}
+			}
+		}
+	}
+
+}
+int main()
+{
+	string str="((2+1*4)*5)/6+(cos(0.123891298)*2+sin(0))^2+sgn(-5)+log(exp)";
+	bool check_for_bracekts=1;
+	cout << "our txt is :" << str << endl;
+	/*
+	find '(' , rembeber it's index and delete, rfind ')' and delete 
+	if found then check check_for_bracekts=1
+	else =0;
+	in the end of whole return
+
+posL = str.find(Lbracket);
+cout << posL << endl;
+str_check_for_brackets.erase(posL, 1);
+cout << str_check_for_brackets;
+*/
+	check_for_bracekts=check_for_correct_input(str, check_for_bracekts);
+	if (check_for_bracekts == 0)
+	{
+		//	throw out_of_range("wrong number of ( and ) brakets");
+		cout << "bruda u make some wrong calculation\n";	
+	}
+	else
+	{
+		cout << "everthing is okey\n";
+		calculation(str);
+	}
+
+}
